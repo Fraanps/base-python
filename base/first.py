34116e8ed5606d49b898426d92a52a5b022ea0a8
Francilene Silva <psilva.francilene@gmail.com>
2025-01-28 17:27:37 +0000

    feat: tipos de dados elementares em python

---
+++ base/first.py
@@ -0,0 +1,9 @@
+
+print('Hello world')
+print('Aprendendo Python')
+# Comentário de uma única linha
+
+"""
+    Comentário em 
+    mais de uma linha
+"""
---
+++ base/tipo-dados.py
@@ -0,0 +1,16 @@
+# Tipos de dados elementares
+# Pythonflix
+
+nome = "Top Gun Maverick"
+ano_lancamento = 2023
+nota_filme = 9.5
+incluso_plano = False
+
+print(nome)
+print(ano_lancamento)
+
+# função para verificar o tipo de dado das variáveis
+print(type(nome))
+print(type(ano_lancamento))
+print(type(nota_filme))
+print(type(incluso_plano))
